e9492085c5d26a77683f3ebac3f91c6d03ca147c
Huy <huy>
2025-06-15 13:13:57 +0700

    feat> bew IP logs for better analytics

--- server.ts
+++ server.ts
@@ -5,9 +5,9 @@ import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import useragent from 'useragent';
 
-import musixmatch from './search_engine/musixmatch/router';
+// import musixmatch from './search_engine/musixmatch/router';
 import genius from './search_engine/genius/router';
-import youtube from './search_engine/youtube/router';
+// import youtube from './search_engine/youtube/router';
 import log from "./utils/logger";
 
 
@@ -21,8 +21,15 @@ function logDetails(req: Request, res: Response, next: NextFunction) {
     const browserDetails = `${agent.toAgent()} on ${agent.os}`;
     const ipAddress = req.ip;
 
+    const realIP = req.headers['x-forwarded-for'] || req.ip;
+    const origin = req.headers.origin || 'Direct/No Origin';
+    const referer = req.headers.referer || 'Direct/No Referer';
+
     log.success(`[${currentTime}] Request: ${req.method} ${req.originalUrl}`);
-    log.warn(`[${currentTime}] User-Agent: ${browserDetails} | IP: ${ipAddress}`);
+    // log.warn(`[${currentTime}] User-Agent: ${browserDetails} | IP: ${ipAddress}`);
+    log.warn(`[${currentTime}] User-Agent: ${browserDetails}`);
+    log.info(`[${currentTime}] IP: ${ipAddress} | Real IP: ${realIP}`);
+    log.info(`[${currentTime}] Origin: ${origin} | Referer: ${referer}`);
     next();
 }
 
